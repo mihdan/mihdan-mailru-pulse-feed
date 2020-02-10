8d0f28910bc23627f4c6964f56a13178dec35be6
Mikhail Kobzarev <mihdan@yandex.ru>
2020-02-10 21:39:49 +0300

    Add uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -7,6 +7,7 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 delete_option( 'wptrt_notice_dismissed_mihdan-mailru-pulse-feed' );
+delete_option( 'mihdan_mailru_pulse_feed_review_dismissed' );
 delete_option( 'feed' );
 delete_option( 'source' );
 delete_option( 'widget' );
