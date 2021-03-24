3ec2c872e496434ad950cce4954d5ae1e980bd4b
mihdan <mikhail@kobzarev.com>
2021-03-24 14:56:19 +0300

    Add placeholder for textarea

--- includes/class-wp-osa.php
+++ includes/class-wp-osa.php
@@ -550,7 +550,7 @@ class WP_OSA {
 		$value = esc_textarea( $this->get_option( $args['id'], $args['section'], $args['std'] ) );
 		$size  = isset( $args['size'] ) && ! is_null( $args['size'] ) ? $args['size'] : 'regular';
 
-		$html  = sprintf( '<textarea rows="5" cols="55" class="%1$s-text" id="%2$s[%3$s]" name="%2$s[%3$s]">%4$s</textarea>', $size, $args['section'], $args['id'], $value );
+		$html  = sprintf( '<textarea rows="5" cols="55" class="%1$s-text" id="%2$s[%3$s]" name="%2$s[%3$s]" placeholder="%4$s">%5$s</textarea>', $size, $args['section'], $args['id'], $args['placeholder'], $value );
 		$html .= $this->get_field_description( $args );
 
 		echo $html;
