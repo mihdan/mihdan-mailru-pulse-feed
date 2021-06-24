9ddfd03891b19f1aeb8e6b4df0695d3f79add1fa
mihdan <mikhail@kobzarev.com>
2021-06-24 16:06:46 +0300

    Bump up version

--- mihdan-mailru-pulse-feed.php
+++ mihdan-mailru-pulse-feed.php
@@ -7,10 +7,10 @@
  * Description: Плагин, формирует ленту для новой рекомендательной системы Пульс от компании Mail.ru. Пульс создает персонализованный контент на базе технологий машинного обучения.
  * Author: Mikhail Kobzarev
  * Author URI: https://www.kobzarev.com/
- * Requires at least: 5.0
+ * Requires at least: 5.3
  * Tested up to: 5.6
- * Version: 0.3.23
- * Stable tag: 0.3.23
+ * Version: 0.3.24
+ * Stable tag: 0.3.24
  *
  * Text Domain: mihdan-mailru-pulse-feed
  * Domain Path: /languages/
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'MIHDAN_MAILRU_PULSE_FEED_VERSION', '0.3.23' );
+define( 'MIHDAN_MAILRU_PULSE_FEED_VERSION', '0.3.24' );
 define( 'MIHDAN_MAILRU_PULSE_FEED_PATH', __DIR__ );
 define( 'MIHDAN_MAILRU_PULSE_FEED_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 define( 'MIHDAN_MAILRU_PULSE_FEED_FILE', __FILE__ );
