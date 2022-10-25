d41d9932ae2c0be40c8086c0b610a2b92423a174
mihdan <mikhail@kobzarev.com>
2022-10-25 16:39:39 +0300

    Update composer requrements

--- mihdan-mailru-pulse-feed.php
+++ mihdan-mailru-pulse-feed.php
@@ -9,8 +9,8 @@
  * Author URI: https://www.kobzarev.com/
  * Requires at least: 5.3
  * Tested up to: 6.0
- * Version: 0.5.0.2
- * Stable tag: 0.5.0.2
+ * Version: 0.5.0.3
+ * Stable tag: 0.5.0.3
  *
  * Text Domain: mihdan-mailru-pulse-feed
  * Domain Path: /languages/
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'MIHDAN_MAILRU_PULSE_FEED_VERSION', '0.5.0.2' );
+define( 'MIHDAN_MAILRU_PULSE_FEED_VERSION', '0.5.0.3' );
 define( 'MIHDAN_MAILRU_PULSE_FEED_PATH', __DIR__ );
 define( 'MIHDAN_MAILRU_PULSE_FEED_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 define( 'MIHDAN_MAILRU_PULSE_FEED_FILE', __FILE__ );
